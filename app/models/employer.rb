12331ea649e8cf30b07457832f4f8d5c7a056d58
dbc-apprentice <apprentice@devbootcamp.com>
2013-06-13 21:06:30 -0500

    updated models, migrations, and seed file

--- app/models/employer.rb
+++ app/models/employer.rb
@@ -1,5 +1,5 @@
 class Employer < ActiveRecord::Base
   belongs_to :company
   belongs_to :user
-  # attr_accessible :title, :body
+  attr_accessible :company_id, :user_id
 end
--- app/models/interest.rb
+++ app/models/interest.rb
@@ -1,5 +1,6 @@
 class Interest < ActiveRecord::Base
-  belongs_to :employer
-  belongs_to :student
-  attr_accessible :approved, :who_initiated
+  belongs_to :employer, :class_name => "User"
+  belongs_to :student, :class_name => "User"
+  belongs_to :who_initiated, :class_name => "User"
+  attr_accessible :approved, :who_initiated_id, :employer_id, :student_id
 end
