a70fb76c9058d8ef6a79892daf59411230f91bd8
dbc-apprentice <apprentice@devbootcamp.com>
2013-06-17 17:40:42 -0500

    updated interest mailer

--- app/mailers/interest_mailer.rb
+++ app/mailers/interest_mailer.rb
@@ -1,16 +1,14 @@
 class InterestMailer < ActionMailer::Base
   default from: "dbcfinalproject@gmail.com" 
 
-
-  def student_initiated_email(catcher,pitcher,responses)
+  def student_initiated_email(catcher,pitcher)
     time = Time.new
     full_time= time.strftime("%A %B %d")
     @time = "#{full_time}"
     @catcher = catcher
     @pitcher = pitcher
-    @responses = responses
     @url = log_in_path
-    mail(:to => "Booneteam@gmail.com", :subject => "Someone's Interested")
+    mail(:to => catcher.email, :reply_to => pitcher.email, :subject => "Someone's Interested")
   end
 
   def employer_initiated_email(catcher,pitcher)
@@ -20,7 +18,7 @@ class InterestMailer < ActionMailer::Base
     @catcher = catcher
     @pitcher = pitcher
     @url  = log_in_path
-    mail(:to => catcher.email, :subject => "Someone's Interested")
+    mail(:to => catcher.email, :reply_to => pitcher.email, :subject => "Someone's Interested")
   end 
 
   def s2s_pending_connection(catcher,pitcher)
@@ -30,7 +28,7 @@ class InterestMailer < ActionMailer::Base
     @catcher = catcher
     @pitcher = pitcher
     @url = log_in_path
-    mail(:to => catcher.email, :subject => "Another DBC Alumni Wants to Connect With You.")
+    mail(:to => catcher.email, :reply_to => pitcher.email , :subject => "Another DBC Alumni Wants to Connect With You.")
   end
 
 end
