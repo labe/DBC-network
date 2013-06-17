1c9cf00fc002d0ef69df513d10cbd57897d8aaec
Booneteam <boone.garrett@gmail.com>
2013-06-17 13:35:06 -0500

    add reply_to field correct pitcher

--- app/mailers/interest_mailer.rb
+++ app/mailers/interest_mailer.rb
@@ -1,15 +1,14 @@
 class InterestMailer < ActionMailer::Base
   default from: "dbcfinalproject@gmail.com" 
 
-
   def student_initiated_email(catcher,pitcher)
     time = Time.new
     full_time= time.strftime("%A %B %d")
     @time = "#{full_time}"
     @catcher = catcher
-    @pitcher = pitcher
+    @pitcher = pitcher.email
     @url = log_in_path
-    mail(:to => catcher.email, :subject => "Someone's Interested")
+    mail(:to => catcher.email, :reply_to => pitcher.email, :subject => "Someone's Interested")
   end
 
   def employer_initiated_email(catcher,pitcher)
@@ -19,7 +18,7 @@ class InterestMailer < ActionMailer::Base
     @catcher = catcher
     @pitcher = pitcher
     @url  = log_in_path
-    mail(:to => catcher.email, :subject => "Someone's Interested")
+    mail(:to => catcher.email, :reply_to => pitcher.email, :subject => "Someone's Interested")
   end 
 
   def s2s_pending_connection(catcher,pitcher)
@@ -29,7 +28,7 @@ class InterestMailer < ActionMailer::Base
     @catcher = catcher
     @pitcher = pitcher
     @url = log_in_path
-    mail(:to => catcher.email, :subject => "Another DBC Alumni Wants to Connect With You.")
+    mail(:to => catcher.email, :reply_to => pitcher.email , :subject => "Another DBC Alumni Wants to Connect With You.")
   end
 
 end
