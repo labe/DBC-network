4becba15dff8fca27e6477828139bff931de4f8f
Henry Wang <henry.t.wang@gmail.com>
2013-06-18 08:32:25 -0500

    update interest model

--- app/models/interest.rb
+++ app/models/interest.rb
@@ -15,5 +15,5 @@ class Interest < ActiveRecord::Base
   belongs_to :pitcher, :class_name => "User", :foreign_key => "pitcher_id"
   belongs_to :catcher, :class_name => "User", :foreign_key => "catcher_id"
 
-  attr_accessible :approved, :pitcher_id, :catcher_id, :email_sent
+  attr_accessible :approved, :pitcher_id, :catcher_id, :email_sent_on
 end
